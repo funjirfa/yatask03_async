2b7d591eede4f44b82d734f1d0bdd123110de850
Anton <racer-loser@mail.ru>
2021-10-15 22:14:29 +0300

    async commit

---
+++ solution/index.js
@@ -0,0 +1,66 @@
+module.exports = function (Homework) {
+  const { add, less, equal } = Homework;
+
+  const asyncArrayLength = (arr) => {
+    return new Promise((resolve) => {
+      arr.length((result) => resolve(result))
+    })
+  }
+
+  const asyncArrayGet = (arr, index) => {
+    return new Promise((resolve) => {
+      arr.get(index, (result) => resolve(result))
+    })
+  }
+
+  const asyncEqual = (a, b) => {
+    return new Promise((resolve) => {
+      equal(a, b, (result) => resolve(result))
+    })
+  }
+
+  const asyncLess = (a, b) => {
+    return new Promise((resolve) => {
+      less(a, b, (result) => resolve(result))
+    })
+  }
+
+  const asyncAdd = (a, b) => {
+    return new Promise((resolve) => {
+      add(a, b, (result) => resolve(result))
+    })
+  }
+
+  const asyncFunction = (a, b, fn) => {
+    return new Promise((resolve) => {
+      fn(a, b, (result) => resolve(result))
+    })
+  }
+
+  const reducer = async (asyncArray, fn, initialValue) => {
+    const len = await asyncArrayLength(asyncArray)
+    const empty = await asyncEqual(len, 0)
+    if (empty) {
+      return initialValue
+    }
+
+    let result = initialValue
+    let index = 0
+    let condition = await asyncLess(index, len)
+    while (condition) {
+      const value = await asyncArrayGet(asyncArray, index)
+      result = await asyncFunction(result, value, fn)
+
+      index = await asyncAdd(index, 1)
+      condition = await asyncLess(index, len)
+    }
+
+    return result
+  }
+
+  return (array, fn, initialValue, cb) => {
+    reducer(array, fn, initialValue).then((result) => {
+      cb(result)
+    })
+  }
+}
