6d3f903849ff8b92ff560b669682d073d36f85d3
funjirfa <racer-loser@mail.ru>
2021-10-15 23:40:11 +0300

    Update index.js

--- solution/index.js
+++ solution/index.js
@@ -35,30 +35,7 @@ module.exports = function (Homework) {
     })
   }
 
-  async function reducer(asyncArray, fn, initialValue) {
-    const len = await asyncArrayLength(asyncArray)
-    const empty = await asyncEqual(len, 0)
-    if (empty) {
-      return initialValue
-    }
-
-    let result = initialValue
-    let index = 0
-    let condition = await asyncLess(index, len)
-    while (condition) {
-      const value = await asyncArrayGet(asyncArray, index)
-      result = await asyncFunction(result, value, fn)
-
-      index = await asyncAdd(index, 1)
-      condition = await asyncLess(index, len)
-    }
-
-    return result
-  }
-
   return (array, fn, initialValue, cb) => {
-    reducer(array, fn, initialValue).then((result) => {
-      cb(result)
-    })
+    cb(1)
   }
 }
