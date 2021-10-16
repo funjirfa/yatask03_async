5cdf640c6a93c6b16ef8a3c3b7b4c77d37fd896d
funjirfa <racer-loser@mail.ru>
2021-10-16 11:37:21 +0300

    Update index.js

--- solution/index.js
+++ solution/index.js
@@ -42,6 +42,7 @@ module.exports = function (Homework) {
     let condition = await asyncLess(index, len)
     while (condition) {
       const value = await asyncArrayGet(array, index)
+      result = await asyncFunction(result, value, fn)
       index = await asyncAdd(index, 1)
       condition = await asyncLess(index, len)
     }
