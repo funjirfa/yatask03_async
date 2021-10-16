5ca636c10988457d91d88babba1a534de95563ea
funjirfa <racer-loser@mail.ru>
2021-10-16 11:00:53 +0300

    Update index.js

--- solution/index.js
+++ solution/index.js
@@ -1,53 +1,41 @@
 module.exports = function (Homework) {
   function asyncArrayLength(arr) {
-    return new Promise((resolve) => {
-      arr.length((result) => resolve(result))
+    return new Promise(function(resolve) {
+      arr.length(function(result) { resolve(result) })
     })
   }
 
   function asyncArrayGet(arr, index) {
-    return new Promise((resolve) => {
-      arr.get(index, (result) => resolve(result))
+    return new Promise(function(resolve) {
+      arr.get(index, function(result) { resolve(result) })
     })
   }
 
   function asyncEqual(a, b) {
-    return new Promise((resolve) => {
-      Homework.equal(a, b, (result) => resolve(result))
+    return new Promise(function(resolve) {
+      Homework.equal(a, b, function(result) { resolve(result) })
     })
   }
 
   function asyncLess(a, b) {
-    return new Promise((resolve) => {
-      Homework.less(a, b, (result) => resolve(result))
+    return new Promise(function(resolve) {
+      Homework.less(a, b, function(result) { resolve(result) })
     })
   }
 
   function asyncAdd(a, b) {
-    return new Promise((resolve) => {
-      Homework.add(a, b, (result) => resolve(result))
+    return new Promise(function(resolve) {
+      Homework.add(a, b, function(result) { resolve(result) })
     })
   }
 
   function asyncFunction(a, b, fn) {
-    return new Promise((resolve) => {
-      fn(a, b, (result) => resolve(result))
+    return new Promise(function(resolve) {
+      fn(a, b, function(result) { resolve(result) })
     })
   }
   
   return async function (array, fn, initialValue, cb) {
-    const len = await asyncArrayLength(array)
-    let result = initialValue
-    let index = 0
-    let condition = await asyncLess(index, len)
-    while (condition) {
-      const value = await asyncArrayGet(array, index)
-      result = await asyncFunction(result, value, fn)
-
-      index = await asyncAdd(index, 1)
-      condition = await asyncLess(index, len)
-    }
-
-    cb(result)
+    cb(10)
   }
 }
